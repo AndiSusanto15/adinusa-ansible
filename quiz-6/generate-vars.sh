b7146a56e12d7c12ccfb0b8d255f4ba96fa3b43c
Andi <andi@digitalsekuriti.id>
2023-05-18 14:32:51 +0700

    init

---
+++ quiz-6/generate-vars.sh
@@ -0,0 +1,6 @@
+echo -e \ \-\ ops{1..50}"\n" > managed1host-batutahid.yml
+echo -e \ \-\ dev{1..50}"\n" >> managed1-host-batutahid.yml
+
+echo -e \ \-\ ops{51..100}"\n" > managed2-host-batutahid.yml
+echo -e \ \-\ dev{51..100}"\n" >> managed2-host-batutahid.yml
+
